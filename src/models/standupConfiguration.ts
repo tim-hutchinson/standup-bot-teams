4631f95508df291958dbde58c133eedbdf87962d
Tim Hutchinson <strike.z@gmail.com>
2017-11-23 21:26:03 -0500

    Define beginnings of configuration and response models

---
+++ src/models/standupConfiguration.ts
@@ -0,0 +1,19 @@
+import * as dynogels from 'dynogels';
+import * as Joi from 'joi';
+
+// tslint:disable-next-line variable-name
+const StandupConfiguration = dynogels.define('StandupConfiguration', {
+  hashKey: 'teamID',
+  rangeKey: 'configID',
+  timestamps: true,
+  schema: {
+    configID: dynogels.types.uuid(),
+    teamID: Joi.string(),
+    createdBy: Joi.string(),
+    reportChannel: Joi.string(),
+    reportTime: Joi.string(), // Time of day only
+    members: dynogels.types.stringSet(),
+  },
+});
+
+export default StandupConfiguration;
---
+++ src/models/standupResponse.ts
@@ -0,0 +1,44 @@
+import * as dynogels from 'dynogels';
+import * as Joi from 'joi';
+
+// tslint:disable-next-line variable-name
+const StandupResponse = dynogels.define('StandupResponse', {
+  hashKey: 'userID',
+  rangeKey: 'responseDate',
+  timestamps: true,
+  schema: {
+    userID: Joi.string(),
+    teamID: Joi.string(),
+    standupConfigID: dynogels.types.uuid(),
+    responseDate: Joi.date(),
+    didRespond: Joi.boolean(),
+    responses: {
+      didYesterday: Joi.string(),
+      doToday: Joi.string(),
+      blockers: Joi.string(),
+    },
+  },
+  indexes: [
+    {
+      hashKey: 'standupConfigID',
+      rangeKey: 'responseDate',
+      name: 'StandupByDayIndex',
+      type: 'global',
+    },
+  ],
+});
+
+export interface IStandupResponse {
+  userID: string;
+  teamID: string;
+  standupConfigID: string;
+  responseDate: Date;
+  didRespond: boolean,
+  responses: {
+    didYesterday?: string;
+    doToday?: string;
+    blockers?: string;
+  };
+}
+
+export default StandupResponse;
